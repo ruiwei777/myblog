b55b59bb224ff260ed634b6c6caa75b015dc42df
ruiwei <liangruiwei777@gmail.com>
2017-07-25 16:52:13 +1000

    Rework index page

--- src/trydjango19/settings/__init__.py
+++ src/trydjango19/settings/__init__.py
@@ -3,11 +3,11 @@ from .base import *
 try:
 	from .local_settings import *
 except Error as e:
-	print("Import local_settings failed.")
+	print("No local settings detected. You can optionally create a local_settings.py file under src/trydjango19/settings/ .")
 	print(e)
 
 try:
 	from .production_settings import *
 except Error as e:
-	print("Import production_settings failed.")
+	print("No production settings detected. You can optionally create a production_settings.py file under src/trydjango19/settings/ .")
 	print(e)
