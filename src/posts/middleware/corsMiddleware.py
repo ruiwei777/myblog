400ad8b1f0ae4f77239b9844f4ef495647813163
ruiwei <liangruiwei777@gmail.com>
2017-08-11 14:16:46 +1000

    fix CORS OPTIONS preflight request

--- src/posts/middleware/corsMiddleware.py
+++ src/posts/middleware/corsMiddleware.py
@@ -1,4 +1,13 @@
+
 class corsMiddleware(object):
     def process_response(self, req, resp):
+
+        # fix the CORS OPTIONS preflight
+        if req.method == "OPTIONS" and req.path_info == "/api-token-auth/":
+          if req.META["HTTP_ACCESS_CONTROL_REQUEST_METHOD"] == "POST" and req.META["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"] == "content-type":
+            resp["Access-Control-Allow-Headers"] = "content-type"
+            resp["Access-Control-Allow-Methods"] = "POST"
+
         resp["Access-Control-Allow-Origin"] = "*"
+
         return resp
