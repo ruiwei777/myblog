73125cf1e68fb6d54a271740f339363e440d64d1
Ruiwei Liang <liangruiwei777@gmail.com>
2019-08-11 18:57:30 +1000

    Fix webpack prod compile. Update deprecated plugins.

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,53 +2,34 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
 
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const extractCSS = new ExtractTextPlugin("css/[name]-[contenthash]-css.css");
-const extractSASS = new ExtractTextPlugin("css/[name]-[contenthash]-sass.css");
-
-
-
-common.devtool = 'source-map';
-
-common.module.rules.push(
-  {
-    test: /\.css$/,
-    use: extractCSS.extract({
-      use: [{ loader: 'css-loader', options: { sourceMap: true } }],
-    }),
-    exclude: /node_modules/
-  },
-  {
-    test: /\.scss$/,
-    use: extractSASS.extract({
-      use: [
-        { loader: 'css-loader', options: { sourceMap: true } },
-        { loader: 'sass-loader', options: { sourceMap: true } }
-      ]
-    }),
-    exclude: /node_modules/
-  }
-);
-
-common.plugins.push(
-  extractCSS,
-  extractSASS,
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new UglifyJSPlugin({
-    sourceMap: true
-  }),
-  new webpack.optimize.ModuleConcatenationPlugin()
-);
-
-console.log("Production mode...");
-
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name]-[hash].css',
+      chunkFilename: '[id].css',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+  ]
 });
+
+console.log("Production mode...");
